83b8098dcfcdd2021398ae5cf72c7b2216ca0aed
Orçun Or <orcun.or@triatech.com.tr>
2022-08-22 14:04:12 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Domain/Entities/SomeFeatureEntity.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SomeFeatureEntity : Entity
+    {
+        public string Name { get; set; }
+        public SomeFeatureEntity() // THİS
+        {
+
+        }
+
+        public SomeFeatureEntity(int id , string name) : this() 
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
